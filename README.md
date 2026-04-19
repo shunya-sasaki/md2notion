400d37a3edee5ae4137ee0cc8229b557a556342c
Shunya Sasaki <shunya.sasaki1120@gmail.com>
2026-04-19 12:20:06 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# md2notion
+
+## 📦 Requirements
+
+- Node.js
+
+## ⚙️ Setup
+
+## 🚀 Usage
+
+## 📚 Reference
+
+## 📄 License
+
+MIT license. See LICENSE[./LICENSE] for more details.
---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Node.js development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        nodeVersion = pkgs.nodejs_25;
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            packages = [
+              nodeVersion
+              nodeVersion.pkgs.yarn
+              nodeVersion.pkgs.pnpm
+            ];
+            shellHook = ''
+              echo "Node.js ${nodeVersion.version} environment"
+            '';
+          };
+      }
+    );
+}
